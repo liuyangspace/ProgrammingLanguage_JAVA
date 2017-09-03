508f430aa258becb395f45eabfac952124c94129
liuyangspace <2260597513@qq.com>
2017-09-03 10:21:49 +0800

    START

---
+++ Java/LanguageStatement/Keyword.java
@@ -0,0 +1,13 @@
+
+DLL(Dynamic Link Library（动态链接库）)
+API(Application Programming Interface，应用编程接口)
+SDK(Software Development Kit,软件开发工具包)
+JDK(Java Development Kit是 Java 语言的软件开发工具包(SDK))
+	(JDK 1.1.4~JDK 1.1.8 ~ J2SE 1.2~1.4.2 ~ Java SE 5.0 (1.5.0)~8.0 (1.8.0))
+	J2SE(Java 2 Standard Edition,从JDK 5.0开始，改名为Java SE）
+	J2EE(Java 2 Enterprise Edition)(Java EE)
+	J2ME(Java 2 Micro Edition)(Java ME)
+JRE(Java Runtime Environment，Java运行环境)
+	JVM(Java Virtual Machine（Java虚拟机）)(虚机器码（Virtual Machine Code）)
+JSR(Java Specification Requests,Java 规范提案)
+JCP(Java Community Process,Java开发者以及被授权者组成，职能是发展和更新)
---
+++ Java/test/LanguageStatement/DataType/DataType.java
@@ -0,0 +1,20 @@
+import java.*;
+
+public class DataType
+{
+	// constant 
+	final int a=345;
+	//final float b=33.2;
+	final double c=33.2d;
+	final String d="ddddd";
+	// variable
+	int a1,b2,c3;
+	int a5=6,b9=8;
+	//float tr=33.;
+	double sxf$;
+	String _s44="dddd";
+
+	public static void main(String arg[]){
+		System.out.println("Hello World!");
+	}
+}
